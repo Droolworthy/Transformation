9eed8834e335bebbdbfa5825056f3d224db450ac
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2023-12-04 12:16:15 +0300

    Update Rotation.cs

--- Rotation.cs
+++ Rotation.cs
@@ -1,35 +1,44 @@
+using System.Collections;
 using UnityEngine;
 
-[RequireComponent (typeof(Animator))]
-public class AttackState : State
+public class Rotation : MonoBehaviour
 {
-    [SerializeField] private int _damage;
-    [SerializeField] private float _delay;
+    [SerializeField] private float _rotationTime;
+    [SerializeField] private float _rotationSpeed;
+    [SerializeField] private float _timeWait;
 
-    private float _lastAssaultTime;
-    private Animator _animator;
+    private Coroutine _coroutine;
 
     private void Start()
     {
-        _animator = GetComponent<Animator>();
+        if (_coroutine != null)
+            StopCoroutine(_coroutine);
+
+        _coroutine = StartCoroutine(WaitForTime(_timeWait));
     }
 
-    private void Update()
+    private IEnumerator WaitForTime(float delay)
     {
-        if(_lastAssaultTime <= 0)
-        {
-            Assault(Target);
+        var wait = new WaitForSeconds(delay);
 
-            _lastAssaultTime = _delay;
+        yield return wait;
+
+        _coroutine = StartCoroutine(TransformObject(_rotationTime));
+    }
+
+    private IEnumerator TransformObject(float duration)
+    {
+        float time = 0;
+
+        while (time < duration)
+        {
+            transform.Rotate(0, _rotationSpeed, 0);
+
+            time += Time.deltaTime;
+
+            yield return null;
         }
 
-        _lastAssaultTime -= Time.deltaTime;  
-    }
-
-    private void Assault(Player target)
-    {
-        _animator.Play("Assault");
-
-        target.ApplyDamage(_damage);
+        StopCoroutine(_coroutine);       
     }
 }
